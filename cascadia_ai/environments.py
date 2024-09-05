a7733d95ba3bcb641f2b7d4ebe27198ff6acc072
David Moody <david@davidxmoody.com>
2024-09-05 15:00:38 +0100

    Add hex grid class

--- cascadia_ai/environments.py
+++ cascadia_ai/environments.py
@@ -1,11 +1,7 @@
 from cascadia_ai.enums import Wildlife
+from cascadia_ai.hex_grid import HexGrid
 from cascadia_ai.tiles import Tile
 from typing import NamedTuple
-
-
-class HexPosition(NamedTuple):
-    q: int
-    r: int
 
 
 class RotatedTile(NamedTuple):
@@ -13,19 +9,21 @@ class RotatedTile(NamedTuple):
     rotation: int
 
 
-starting_tiles: list[dict[HexPosition, RotatedTile]] = [
-    {
-        HexPosition(0, 0): RotatedTile(Tile.from_definition("MMb"), 0),
-        HexPosition(0, -1): RotatedTile(Tile.from_definition("FWehf"), 1),
-        HexPosition(1, -1): RotatedTile(Tile.from_definition("RPbs"), 2),
-    }
+starting_tiles: list[HexGrid[RotatedTile]] = [
+    HexGrid(
+        {
+            (0, 0): RotatedTile(Tile.from_definition("MMb"), 0),
+            (0, -1): RotatedTile(Tile.from_definition("FWehf"), 1),
+            (1, -1): RotatedTile(Tile.from_definition("RPbs"), 2),
+        }
+    ),
 ]
 
 
 class Environment:
-    tiles: dict[HexPosition, RotatedTile]
-    wildlife: dict[HexPosition, Wildlife]
+    tiles: HexGrid[RotatedTile]
+    wildlife: HexGrid[Wildlife]
 
-    def __init__(self, starting_tile_group: dict[HexPosition, RotatedTile]):
+    def __init__(self, starting_tile_group: HexGrid[RotatedTile]):
         self.tiles = starting_tile_group.copy()
-        self.wildlife = {}
+        self.wildlife = HexGrid()
---
+++ cascadia_ai/hex_grid.py
@@ -0,0 +1,32 @@
+from typing import Generic, TypeVar, NamedTuple
+
+
+type HexPosition = tuple[int, int]
+
+hex_steps = [(1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1), (0, 1)]
+
+
+T = TypeVar("T")
+
+
+class HexGrid(Generic[T]):
+    _data: dict[HexPosition, T]
+
+    def __init__(self, data: None | dict[HexPosition, T] = None):
+        self._data = data or {}
+
+    def __getitem__(self, key: HexPosition):
+        return self._data[key] if key in self._data else None
+
+    def __setitem__(self, key: HexPosition, value: T):
+        if key in self._data:
+            raise KeyError("Cannot overwrite existing data")
+        self._data[key] = value
+
+    def copy(self):
+        return HexGrid(self._data.copy())
+
+    def adjacent(self, key: HexPosition):
+        q, r = key
+        for dq, dr in hex_steps:
+            yield (q + dq, r + dr)
