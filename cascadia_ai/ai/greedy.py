e76c8ee56f2a9a6bbf0617360135914a728841e9
David Moody <david@davidxmoody.com>
2024-09-30 14:05:16 +0100

    Update greedy algorithm to use new actions

--- cascadia_ai/ai/greedy.py
+++ cascadia_ai/ai/greedy.py
@@ -1,15 +1,16 @@
 from random import choice
-from cascadia_ai.ai.transitions import get_transitions
+from cascadia_ai.ai.actions import get_actions
 from cascadia_ai.game_state import GameState
 
 
 def play_game(state: GameState, until_turns_remaining=0):
     while state.turns_remaining > until_turns_remaining:
-        actions, rewards, _ = get_transitions(state)
-        max_reward = max(rewards)
-        max_actions = [
-            action for action, reward in zip(actions, rewards) if reward == max_reward
-        ]
-        action = choice(max_actions)
+        actions_and_rewards = list(get_actions(state))
+
+        max_reward = max(r for _, r in actions_and_rewards)
+        max_actions = [a for a, r in actions_and_rewards if r == max_reward]
+        max_actions_no_nt = [a for a in max_actions if a.tile_index == a.wildlife_index]
+
+        action = choice(max_actions_no_nt if len(max_actions_no_nt) else max_actions)
         state.take_action(action)
     return state
